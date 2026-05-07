8c9ecf728cac5bd089de4c05af47412d4e4431e6
eriklaurenti <seuemaildopgithub@gmail.com>
2026-05-07 18:57:17 -0300

    erik editou 22:40

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,31 @@
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+<<<<<<< HEAD
   { 
     ignores: ["**/*"],
+=======
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+>>>>>>> 433cf70 (erik editou 22:40)
   }
 );
